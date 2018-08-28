1c635baa65937b84650903f1c8b93945eec9a6eb
Y-YoL <11359025+Y-YoL@users.noreply.github.com>
2018-08-28 21:48:19 +0900

    Initial commit

---
+++ UniCs2Cpp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniCs2Cpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
